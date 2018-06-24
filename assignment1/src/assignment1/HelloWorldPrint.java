c0aa499cf27b42ab3ff25d26dda85f285096ddc7
rajan <rajan@LAPTOP-124D7RCR>
2018-06-24 20:23:24 +0530

    HelloWorld Assignment

---
+++ assignment1/src/assignment1/HelloWorldPrint.java
@@ -0,0 +1,21 @@
+package assignment1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author rajan
+ *
+ */
+public class HelloWorldPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date=new Date();
+		System.out.println(formatter.format(date));
+
+	}
+
+}
